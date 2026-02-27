3d1b9aa526e726e429592895ff295b23123e3aca
Nikhil07 <nikhil.nayak4561@gmail.com>
2026-02-27 14:16:58 +0530

    Add Course model with courseName, courseDescription, instructor, createdAt

---
+++ backend/models/Course.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+const courseSchema = new mongoose.Schema({
+  courseName: {
+    type: String,
+    required: true,
+  },
+  courseDescription: {
+    type: String,
+    required: true,
+  },
+  instructor: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("Course", courseSchema);
